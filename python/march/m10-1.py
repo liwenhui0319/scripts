a92fd54174bd3e5bded436459f1b0ecb5ed13499
liwenhui0319 <1521736836@qq.com>
2023-03-10 17:11:25 +0800

    获取最大值

---
+++ python/march/m10-1.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+scores=[67,78,56,89,76,79,98,45,75,65,76]
+maxScore=scores[0]
+for score in scores:
+    if score > maxScore:
+        maxScore = score
+print(maxScore)
+        
